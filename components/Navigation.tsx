7be0de8043e0b4ddbd472b1279e9924463f087b8
Jan Bauer <bauer.jan123@gmail.com>
2024-12-05 09:24:44 +0100

    fix nav color

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -20,7 +20,7 @@ export default function Navigation() {
                 "py-0.5 px-3 text-xs leading-6 uppercase rounded-full",
                 "ring-1 hover:ring-2 transition-all",
                 "text-neutral-700  dark:text-gray-200  ring-neutral-700 dark:ring-gray-200",
-                isActive && "font-semibold ring-2 bg-neutral-700 text-gray-100 dark:bg-gray-200 dark:!text-[#485563]"
+                isActive && "font-semibold ring-2 bg-neutral-700 !text-gray-100 dark:bg-gray-200 dark:!text-[#485563]"
               )}
             >
               {t(link.name)}
